a198438772ccd9b4234add5dd9a9ffc41a5626bf
luanxu-mch <luanxu@mxc.org>
2025-07-29 09:36:48 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# simple-mxc-node
+
+Detailed steps are here : https://doc.moonchain.com/docs/Testnet-Tutorials/Run-Moonchain-Supernode
+
+1.在main分支执行git pull或者以其他方式更新下载仓库https://github.com/JDI-Group/simple-mxc-node
+2.复制.env.exmaple文件更名为.env，并且修改L1_ENDPOINT_HTTP,L1_ENDPOINT_WS环境变量分别为BNB Smart Chain网络的http rpc和rpc websocket链接
+
+执行"docker compose --profile l2_execution_engine up  -d"命令
+
+Steps:
+
+On the main branch, run git pull or update the repository via other methods: https://github.com/JDI-Group/simple-mxc-node.
+Copy the .env.exmaple file and rename it to .env. Set L1_ENDPOINT_HTTP and L1_ENDPOINT_WS to the HTTP RPC and WebSocket RPC endpoints of the BNB Smart Chain, respectively.
+Run the "docker compose --profile l2_execution_engine up  -d"command.
---
+++ script/start-driver.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -eou pipefail
+
+if [ "$DISABLE_P2P_SYNC" = "false" ]; then
+    exec taiko-client driver \
+        --l1.http "${L1_ENDPOINT_HTTP}" \
+        --l1.ws "${L1_ENDPOINT_WS}" \
+        --l2.ws ws://l2_execution_engine:8546 \
+        --l2.auth http://l2_execution_engine:8551 \
+        --taikoL1 "${TAIKO_L1_ADDRESS}" \
+        --taikoL2 "${TAIKO_L2_ADDRESS}" \
+        --jwtSecret /data/moonchain-geth/geth/jwtsecret \
+        --p2p.sync \
+        --p2p.checkPointSyncUrl "${P2P_SYNC_URL}"
+
+else
+    exec taiko-client driver \
+        --l1.http "${L1_ENDPOINT_HTTP}" \
+        --l1.ws "${L1_ENDPOINT_WS}" \
+        --l2.ws ws://l2_execution_engine:8546 \
+        --l2.auth http://l2_execution_engine:8551 \
+        --taikoL1 "${TAIKO_L1_ADDRESS}" \
+        --taikoL2 "${TAIKO_L2_ADDRESS}" \
+        --jwtSecret /data/moonchain-geth/geth/jwtsecret
+fi
